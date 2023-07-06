ec7b7e53e4e7c55f30a7150e4202e69276bb905b
Pankaj Kumar Meena <pankajkmeena12@gmail.com>
2023-07-06 22:21:15 +0530

    Home Slider Videos APi create and integration

--- fullstack/Backend/src/config/config.js
+++ fullstack/Backend/src/config/config.js
@@ -3,39 +3,36 @@
 
 const dotenv = require("dotenv");
 const path = require("path");
-// const Joi = require("joi");
+const Joi = require("joi");
 
 dotenv.config({ path: path.join(__dirname, "../../.env") });
 
-// const envVarsSchema = Joi.object()
-//   .keys({
-//     NODE_ENV: Joi.string()
-//       .valid("production", "development", "test")
-//       .required(),
-//     PORT: Joi.number().default(3000),
-//     MONGODB_URL: Joi.string().required().description("Mongo DB url"),
-//     JWT_SECRET: Joi.string().required().description("JWT secret key"),
-//   })
-//   .unknown();
+const envVarsSchema = Joi.object()
+  .keys({
+    // PORT: Joi.number().default(3000),
+    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+    JWT_SECRET: Joi.string().required().description("JWT secret key"),
+  })
+  .unknown();
 
-// const { value: envVars, error } = envVarsSchema
-//   .prefs({ errors: { label: "key" } })
-//   .validate(process.env);
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
 
-// if (error) {
-//   throw new Error(`Config validation error: ${error.message}`);
-// }
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
 
 module.exports = {
   port: 3003,
   mongoose: {
-    url: "mongodb+srv://Pankaj1999:jupiter123@jupiter-blog.smfhyei.mongodb.net/jupiter",
+    url: envVars.MONGODB_URL,
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
   },
-  //   jwt: {
-  //     secret: envVars.JWT_SECRET,
-  //   },
+  jwt: {
+    secret: envVars.JWT_SECRET,
+  },
 };
