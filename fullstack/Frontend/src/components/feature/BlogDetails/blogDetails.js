d8ad879928b01639cc49ef03dc9bb67a7bb19723
Pankaj Kumar Meena <pankajkmeena12@gmail.com>
2023-08-23 00:49:21 +0530

    Fix comment Part Css issue

--- fullstack/Frontend/src/components/feature/BlogDetails/blogDetails.js
+++ fullstack/Frontend/src/components/feature/BlogDetails/blogDetails.js
@@ -1,20 +1,12 @@
-import MailIcon from "@mui/icons-material/Mail";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
+import CloseIcon from "@mui/icons-material/Close";
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import * as React from "react";
+import style from "./style";
 
 export default function TemporaryDrawer() {
   const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
     right: false,
   });
 
@@ -31,36 +23,14 @@ export default function TemporaryDrawer() {
 
   const list = (anchor) => (
     <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 340 }}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <Box sx={style.messageSection}>
+        <Typography variant="h1">Responses</Typography>
+        <CloseIcon onClick={toggleDrawer(anchor, false)} />
+      </Box>
     </Box>
   );
 
@@ -75,6 +45,15 @@ export default function TemporaryDrawer() {
             anchor={anchor}
             open={state[anchor]}
             onClose={toggleDrawer(anchor, false)}
+            variant={"persistent"}
+            PaperProps={{
+              sx: {
+                boxShadow: "rgba(0, 0, 0, 0.15) 0px 4px 12px",
+                borderLeft: "0",
+                padding: "25px",
+                background: "rgb(237 249 255)",
+              },
+            }}
           >
             {list(anchor)}
           </Drawer>
