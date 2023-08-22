f27991a4f5f46077ddc27efefedaa7094d7a75ef
Pankaj Kumar Meena <pankajkmeena12@gmail.com>
2023-08-23 00:11:32 +0530

    BlogDetails Page Create

--- fullstack/Frontend/pages/[user]/[title].js
+++ fullstack/Frontend/pages/[user]/[title].js
@@ -1,9 +1,11 @@
 import BlogDetailComponent from "../../src/components/feature/Blog/BlogDetailComponent";
+import CommentBlog from "../../src/components/feature/BlogDetails";
 
 export default function UserNamePage() {
   return (
     <div style={{ background: "#000" }}>
-      <BlogDetailComponent />
+      {/* <BlogDetailComponent /> */}
+      <CommentBlog />
     </div>
   );
 }
