8ffa9ddba5b016d0c7832567f44eb08c90676fe0
Pankaj Kumar Meena <pankajkmeena12@gmail.com>
2023-10-18 00:49:28 +0530

    Profile page issue fix

--- src/components/feature/Profile/UserProfile/index.js
+++ src/components/feature/Profile/UserProfile/index.js
@@ -1,15 +1,9 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
-// import style from "../style";
-// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ListingBlog from "../../Blog/ListingBlog";
 
 const UserProfile = () => {
-  return (
-    <>
-     
-    </>
-  );
+  return <>njn</>;
 };
 
 export default UserProfile;
